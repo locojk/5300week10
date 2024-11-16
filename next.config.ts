e1154448838998bda306a1fe0319ad274122cf41
locojk <gaojun5150@gmail.com>
2024-11-15 22:53:45 -0500

    Add project

---
+++ next.config.ts
@@ -0,0 +1,42 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig: import('next').NextConfig = {
+//   async rewrites() {
+//     return [
+//       {
+//         source: "/api/:path*",
+//         destination:
+//           process.env.NODE_ENV === "development"
+//             ? "http://127.0.0.1:8000/api/:path*"
+//             : "/api/",
+//       },
+//     ];
+//   },
+// };
+
+// export default nextConfig;
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig: import('next').NextConfig = {
+//   output: "standalone",
+// };
+
+// export default nextConfig;
+
+/** @type {import('next').NextConfig} */
+const nextConfig: import('next').NextConfig = {
+  output: "standalone",
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination:
+          process.env.NODE_ENV === "development"
+            ? "http://127.0.0.1:8000/api/:path*"
+            : "/api/",
+      },
+    ];
+  },
+};
+
+export default nextConfig;
+
